ddcefab0642572c5c3e62332db0eeb1451764cc7
Nam Tran <nam.tran.ext@personio.de>
2021-08-23 21:31:00 +0700

    Initialize project

---
+++ api/index.go
@@ -0,0 +1,75 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type Update struct {
+	UpdateId string `json:"update_id"`
+	Message  string `json:"message"`
+}
+
+type SendMessageResponse struct {
+	Ok     bool              `json:"ok"`
+	Result SendMessageResult `json:"result"`
+}
+
+type SendMessageResult struct {
+	MessageId int `json:"message_id"`
+}
+
+type PinnedMessage struct {
+	Date int64  `json:"date"`
+	Text string `json:"text"`
+}
+
+var (
+	telegramBotToken = os.Getenv("TELEGRAM_BOT_TOKEN")
+	chatId           = os.Getenv("CHAT_ID")
+	botURL           = fmt.Sprintf("https://api.telegram.org/bot%s", telegramBotToken)
+	client           = http.Client{}
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	var update Update
+
+	err := json.NewDecoder(r.Body).Decode(&update)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+
+		return
+	}
+
+	sendMessage(update.Message)
+}
+
+func sendMessage(text string) (SendMessageResult, error) {
+	request, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf("%s/sendMessage?chat_id=@%s&text=%s", botURL, chatId, text),
+		nil,
+	)
+	if err != nil {
+		return SendMessageResult{}, err
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return SendMessageResult{}, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return SendMessageResult{}, err
+	}
+
+	sendMessageResponse := SendMessageResponse{}
+	json.Unmarshal(body, &sendMessageResponse)
+
+	return sendMessageResponse.Result, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/namtx/rsi-bot-webhook
+
+go 1.15
+
+require golang.org/x/tools/gopls v0.7.1 // indirect
